ce1972a3254cfe55cbde6c52ab2edd7297261ab8
Gevorg Martirosyan <gevorg.martir@gmail.com>
2020-05-30 13:14:12 +0400

    feat(perf): initial load text (#33)

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
@@ -5,7 +6,4 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   watch: true
-  // devServer: {
-  //   contentBase: './dist'
-  // }
 })
